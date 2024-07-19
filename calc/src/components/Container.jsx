7830daba2d8bdd27c1e1660ae162332e5e5f4a05
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-07-19 07:39:21 +0200

    Add footer and Header

--- calc/src/components/Container.jsx
+++ calc/src/components/Container.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Num(props){
     return(
         <>
-            <div className="border-2 p-2 rounded-lg w-10" key={props.number}>{props.number}</div>
+            <div className="border-2 p-2 rounded-lg w-10 hover" key={props.number}>{props.number}</div>
         </>
     )
 }
--- calc/src/components/Footer.jsx
+++ calc/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer(){
     return (
         <>
             <div className="bg-zinc-700 w-full p-1">
-                <p className='text-center text-white text-md m-4'>Created by <a href="http://">Bayanda Dlamini</a> &copy;</p>
+                <p className='text-center text-white text-md m-4'>Created by <a href="http://">&copy; Bayanda Dlamini</a> </p>
             </div>
         </>
     )
